4d3a0cd91fd1cf1cd68b1ab2af7eecb7fa93ae8c
Illia Andreyeu <illia.andreyeu@gmail.com>
2026-03-15 16:44:50 +0100

    Add non-modulo change calculation

--- core/change_calculator.cpp
+++ core/change_calculator.cpp
@@ -3,19 +3,22 @@
 using namespace std;
 
 // Simple program that breaks an amount of cents into coins.
-// Demonstrates integer division and modulo operator.
+// Demonstrates integer division and modulo operator, and an alternative without modulo.
 
-int main() {
+void print_change(int dollars, int quarters, int dimes, int nickels, int pennies, const string& label) {
+    cout << "\n" << label << ":\n";
+    cout << "dollars  : " << dollars << endl;
+    cout << "quarters : " << quarters << endl;
+    cout << "dimes    : " << dimes << endl;
+    cout << "nickels  : " << nickels << endl;
+    cout << "pennies  : " << pennies << endl;
+}
 
+void calculate_change_with_modulo(int cents) {
     const int dollar_value {100};
     const int quarter_value {25};
     const int dime_value {10};
     const int nickel_value {5};
-
-    int cents {};
-
-    cout << "Enter an amount in cents: ";
-    cin >> cents;
 
     int dollars {};
     int quarters {};
@@ -37,12 +40,46 @@ int main() {
 
     pennies = cents;
 
-    cout << "\nChange can be given as:\n";
-    cout << "dollars  : " << dollars << endl;
-    cout << "quarters : " << quarters << endl;
-    cout << "dimes    : " << dimes << endl;
-    cout << "nickels  : " << nickels << endl;
-    cout << "pennies  : " << pennies << endl;
+    print_change(dollars, quarters, dimes, nickels, pennies, "With modulo");
+}
+
+void calculate_change_without_modulo(int cents) {
+    const int dollar_value {100};
+    const int quarter_value {25};
+    const int dime_value {10};
+    const int nickel_value {5};
+
+    int dollars {};
+    int quarters {};
+    int dimes {};
+    int nickels {};
+    int pennies {};
+
+    dollars = cents / dollar_value;
+    cents = cents - (dollars * dollar_value);
+
+    quarters = cents / quarter_value;
+    cents = cents - (quarters * quarter_value);
+
+    dimes = cents / dime_value;
+    cents = cents - (dimes * dime_value);
+
+    nickels = cents / nickel_value;
+    cents = cents - (nickels * nickel_value);
+
+    pennies = cents;
+
+    print_change(dollars, quarters, dimes, nickels, pennies, "Without modulo");
+}
+
+int main() {
+    int cents {};
+
+    cout << "Enter an amount in cents: ";
+    cin >> cents;
+
+    calculate_change_with_modulo(cents);
+    calculate_change_without_modulo(cents);
 
     return 0;
 }
